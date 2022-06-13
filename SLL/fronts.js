5a2b8d9382da6d06e9c7c9b3364fb42bd3904a84
raspuna <raspuna@gmail.com>
2022-06-13 14:09:15 -0700

    display

--- SLL/fronts.js
+++ SLL/fronts.js
@@ -29,6 +29,39 @@ class LinkedList {
     }
     return null;
   }
+  contains(value) {
+    let runner = this.head;
+    while (runner !== null) {
+      if (value === runner.data) {
+        return true;
+      }
+      runner = runner.next;
+    }
+    return false;
+  }
+  length() {
+    let runner = this.head;
+    let cnt = 0;
+    while (runner !== null) {
+      cnt++;
+      runner = runner.next;
+    }
+    return cnt;
+  }
+  display() {
+    let runner = this.head;
+    if (runner === null) {
+      console.log("");
+      return;
+    }
+    let ret_text = "";
+    while (runner.next !== null) {
+      ret_text += runner.data + ", ";
+      runner = runner.next;
+    }
+    ret_text += runner.data;
+    console.log(ret_text);
+  }
 }
 
 s = new LinkedList();
@@ -36,11 +69,24 @@ s.addFront(18);
 console.log(s);
 s.addFront(5);
 s.addFront(73);
-console.log(s);
+console.log("contains 20?", s.contains(20));
+console.log("contains 18?", s.contains(18));
+s.display();
 console.log(s.removeFront());
+s.display();
 s.removeFront();
-console.log(s);
+s.display();
 
 console.log(s.front());
+console.log(s.length());
 console.log(s.removeFront());
 console.log(s.front());
+console.log(s.length());
+
+//====
+s2 = new LinkedList();
+s2.addFront(76);
+s2.addFront(2);
+s2.display();
+s2.addFront(11.41);
+s2.display();
